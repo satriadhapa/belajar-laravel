0f46f12ed833f83eee20be2a96a31d94e39c44cf
apollonsatria <satriadhapa10@gmail.com>
2024-04-02 15:56:55 +0700

    belajar bikin route dan halaman

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about', [
+        'name'  => 'Moch Satria Dhapa Hamdani',
+        'email' => 'satriadhapa10@gmail.com',
+        'image' => 'satria.jpg'
+    ]);
+});
+Route::get('/blog', function () {
+    return view('posts');
 });
